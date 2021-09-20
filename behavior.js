f9a55fccd947a857f46dce941697267abb02d84f
Kai <54748208+Kai-xD13@users.noreply.github.com>
2021-09-19 21:05:06 -0500

    On click trigger and resized keys

--- behavior.js
+++ behavior.js
@@ -1,95 +1,173 @@
+let pressA = document.querySelector(".A");
+let pressS = document.querySelector(".S");
+let pressD = document.querySelector(".D");
+let pressF = document.querySelector(".F");
+let pressG = document.querySelector(".G");
+let pressH = document.querySelector(".H");
+let pressJ = document.querySelector(".J");
+let pressW = document.querySelector(".W");
+let pressE = document.querySelector(".E");
+let pressT = document.querySelector(".T");
+let pressY = document.querySelector(".Y");
+let pressU = document.querySelector(".U");
+
+function interactionA(){
+    let audioA = new Audio("audio/A.mp3");
+    audioA.play();
+    pressA.classList.add("pressWhite");
+    setTimeout(() => pressA.classList.remove("pressWhite"), 500);
+}
+function interactionS(){
+    let audioS = new Audio("audio/S.mp3");
+    audioS.play();
+    pressS.classList.add("pressWhite");
+    setTimeout(() => pressS.classList.remove("pressWhite"), 500);
+}
+function interactionD(){
+    let audioD = new Audio("audio/D.mp3");
+    audioD.play();
+    pressD.classList.add("pressWhite");
+    setTimeout(() => pressD.classList.remove("pressWhite"), 500);
+}
+function interactionF(){
+    let audioF = new Audio("audio/F.mp3");
+    audioF.play();
+    pressF.classList.add("pressWhite");
+    setTimeout(() => pressF.classList.remove("pressWhite"), 500);
+}
+function interactionG(){
+    let audioG = new Audio("audio/G.mp3");
+    audioG.play();
+    pressG.classList.add("pressWhite");
+    setTimeout(() => pressG.classList.remove("pressWhite"), 500);
+}
+function interactionH(){
+    let audioH = new Audio("audio/H.mp3");
+    audioH.play();
+    pressH.classList.add("pressWhite");
+    setTimeout(() => pressH.classList.remove("pressWhite"), 500);
+}
+function interactionJ(){
+    let audioJ = new Audio("audio/J.mp3");
+    audioJ.play();
+    pressJ.classList.add("pressWhite");
+    setTimeout(() => pressJ.classList.remove("pressWhite"), 500);
+}
+function interactionW(){
+    let audioW = new Audio("audio/W.mp3");
+    audioW.play();
+    pressW.classList.add("pressBlack");
+    setTimeout(() => pressW.classList.remove("pressBlack"), 500);
+}
+function interactionE(){
+    let audioE = new Audio("audio/E.mp3");
+    audioE.play();
+    pressE.classList.add("pressBlack");
+    setTimeout(() => pressE.classList.remove("pressBlack"), 500);
+}
+function interactionT(){
+    let audioT = new Audio("audio/T.mp3");
+    audioT.play();
+    pressT.classList.add("pressBlack");
+    setTimeout(() => pressT.classList.remove("pressBlack"), 500);
+}
+function interactionY(){
+    let audioY = new Audio("audio/Y.mp3");
+    audioY.play();
+    pressY.classList.add("pressBlack");
+    setTimeout(() => pressY.classList.remove("pressBlack"), 500);
+}
+function interactionU(){
+    let audioU = new Audio("audio/U.mp3");
+    audioU.play();
+    pressU.classList.add("pressBlack");
+    setTimeout(() => pressU.classList.remove("pressBlack"), 500);
+}
+
 document.addEventListener("keydown", (e)=>{
     e.preventDefault();
     switch (e.code) {
         case "KeyA":
-            let audioA = new Audio("audio/A.mp3");
-            audioA.play();
-            let pressA = document.querySelector(".A");
-            pressA.classList.add("pressWhite");
-            setTimeout(() => pressA.classList.remove("pressWhite"), 500);
+            interactionA();
             break;
         case "KeyS":
-            let audioS = new Audio("audio/S.mp3");
-            audioS.play();
-            let pressS = document.querySelector(".S");
-            pressS.classList.add("pressWhite");
-            setTimeout(() => pressS.classList.remove("pressWhite"), 500);
+            interactionS();
             break;
         case "KeyD":
-            let audioD = new Audio("audio/D.mp3");
-            audioD.play();
-            let pressD = document.querySelector(".D");
-            pressD.classList.add("pressWhite");
-            setTimeout(() => pressD.classList.remove("pressWhite"), 500);
+            interactionD();
             break;
         case "KeyF":
-            let audioF = new Audio("audio/F.mp3");
-            audioF.play();
-            let pressF = document.querySelector(".F");
-            pressF.classList.add("pressWhite");
-            setTimeout(() => pressF.classList.remove("pressWhite"), 500);
+            interactionF();
             break;
         case "KeyG":
-            let audioG = new Audio("audio/G.mp3");
-            audioG.play();
-            let pressG = document.querySelector(".G");
-            pressG.classList.add("pressWhite");
-            setTimeout(() => pressG.classList.remove("pressWhite"), 500);
+            interactionG();
             break;
         case "KeyH":
-            let audioH = new Audio("audio/H.mp3");
-            audioH.play();
-            let pressH = document.querySelector(".H");
-            pressH.classList.add("pressWhite");
-            setTimeout(() => pressH.classList.remove("pressWhite"), 500);
+            interactionH();
             break;
         case "KeyJ":
-            let audioJ = new Audio("audio/J.mp3");
-            audioJ.play();
-            let pressJ = document.querySelector(".J");
-            pressJ.classList.add("pressWhite");
-            setTimeout(() => pressJ.classList.remove("pressWhite"), 500);
+            interactionJ();
             break;
 
         case "KeyW":
-            let audioW = new Audio("audio/W.mp3");
-            audioW.play();
-            let pressW = document.querySelector(".W");
-            pressW.classList.add("pressBlack");
-            setTimeout(() => pressW.classList.remove("pressBlack"), 500);
+            interactionW();
             break;
         case "KeyE":
-            let audioE = new Audio("audio/E.mp3");
-            audioE.play();
-            let pressE = document.querySelector(".E");
-            pressE.classList.add("pressBlack");
-            setTimeout(() => pressE.classList.remove("pressBlack"), 500);
+            interactionE();
             break;
         case "KeyT":
-            let audioT = new Audio("audio/T.mp3");
-            audioT.play();
-            let pressT = document.querySelector(".T");
-            pressT.classList.add("pressBlack");
-            setTimeout(() => pressT.classList.remove("pressBlack"), 500);
+            interactionT();
             break;
         case "KeyY":
-            let audioY = new Audio("audio/Y.mp3");
-            audioY.play();
-            let pressY = document.querySelector(".Y");
-            pressY.classList.add("pressBlack");
-            setTimeout(() => pressY.classList.remove("pressBlack"), 500);
+            interactionY();
             break;
         case "KeyU":
-            let audioU = new Audio("audio/U.mp3");
-            audioU.play();
-            let pressU = document.querySelector(".U");
-            pressU.classList.add("pressBlack");
-            setTimeout(() => pressU.classList.remove("pressBlack"), 500);
+            interactionU();
             break;
 
         default:
             console.warn("That won't work");
             break;
     }
-
 });
+
+pressA.addEventListener("click", () => {interactionA()});
+pressS.addEventListener("click", () => {interactionS()});
+pressD.addEventListener("click", () => {interactionD()});
+pressF.addEventListener("click", () => {interactionF()});
+pressG.addEventListener("click", () => {interactionG()});
+pressH.addEventListener("click", () => {interactionH()});
+pressJ.addEventListener("click", () => {interactionJ()});
+pressW.addEventListener("click", () => {interactionW()});
+pressE.addEventListener("click", () => {interactionE()});
+pressT.addEventListener("click", () => {interactionT()});
+pressY.addEventListener("click", () => {interactionY()});
+pressU.addEventListener("click", () => {interactionU()});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
